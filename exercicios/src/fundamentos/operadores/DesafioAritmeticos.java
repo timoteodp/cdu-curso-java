50178c9ad5817fabc00ae519daba1103dc0f0475
Timóteo Dima Pereira <timoteodp10@outlook.com>
2023-08-28 23:47:51 +0000

    Alteração do Código

--- exercicios/src/fundamentos/operadores/DesafioAritmeticos.java
+++ exercicios/src/fundamentos/operadores/DesafioAritmeticos.java
@@ -4,14 +4,14 @@ public class DesafioAritmeticos {
 
 	public static void main(String[] args) {
 		
-		double numA = Math.pow(6 * (3 + 2), 2);
-		double denA = 3 * 2;
+		double numeradorA = Math.pow(6 * (3 + 2), 2);
+		double denominadorA = 3 * 2;
 		
-		double numB = (1 - 5) * (2 - 7);
-		double denB = 2;
+		double numeradorB = (1 - 5) * (2 - 7);
+		double denominadorB = 2;
 		
-		double superiorA = numA / denA;
-		double superiorB = Math.pow(numB / denB, 2);
+		double superiorA = numeradorA / denominadorA;
+		double superiorB = Math.pow(numeradorB / denominadorB, 2);
 		
 		double superior = Math.pow(superiorA - superiorB, 3);
 		double inferior = Math.pow(10, 3);
